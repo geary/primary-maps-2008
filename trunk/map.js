328a77e279ac439904f0da0a9449edb01d03151c
election.maps.2008@gmail.com <election.maps.2008@gmail.com@d6bc2760-3145-0410-a777-cddf8f24c489>
2008-05-06 01:19:57 +0000

    Support demographic factors for multiple states

--- trunk/map.js
+++ trunk/map.js
@@ -13,4 +13,4 @@ if( ! window.jQuery )
 	scriptIG( 'http://primary-maps-2008.googlecode.com/svn/trunk/jquery-1.2.3-no-ajax.min.js', 3600 );
 	
 scriptIG( 'http://primary-maps-2008.googlecode.com/svn/trunk/map-proto.js', 120 );
-scriptIG( 'http://primary-maps-2008-data.googlecode.com/svn/trunk/demographic/states/pa/demographic.js', 120 );
+scriptIG( 'http://primary-maps-2008-data.googlecode.com/svn/trunk/demographic/demographic.js', 120 );
