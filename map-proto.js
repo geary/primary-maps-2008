3765f3e13643ec74b4b1ba98b0d383ca9ed431a4
election.maps.2008 <election.maps.2008@d6bc2760-3145-0410-a777-cddf8f24c489>
2008-05-21 02:49:40 +0000

    Select KY or OR randomly

--- map-proto.js
+++ map-proto.js
@@ -1722,8 +1722,7 @@ opt.frameUrl = opt.frameUrl || opt.codeUrl;
 opt.dataUrl = opt.dataUrl || 'http://primary-maps-2008-data.googlecode.com/svn/trunk/';
 opt.state = opt.state || 'us';
 if( opt.state == 'zz' )
-	opt.state = 'ky';
-	//opt.state = hotStates.random().replace( '!', '' ).toLowerCase();
+	opt.state = hotStates.random().replace( '!', '' ).toLowerCase();
 
 var state = states[opt.state];
 
