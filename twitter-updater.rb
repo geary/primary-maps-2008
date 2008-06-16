d68c385702f07702d42d20c1742b757d09c524f4
election.maps.2008@gmail.com <election.maps.2008@gmail.com@d6bc2760-3145-0410-a777-cddf8f24c489>
2008-06-16 20:44:00 +0000

    Update for current tweet format

--- twitter-updater.rb
+++ twitter-updater.rb
@@ -16,6 +16,7 @@ require 'xmpp4r-simple'
 class Updater
 	
 	def initialize
+		@JSON = '../election-data/tweets/tweets-new.js'
 		@MAX_UPDATES = 50
 		@lastwrite = Time.now
 		@users = {}
@@ -24,25 +25,23 @@ class Updater
 		@im = Jabber::Simple.new( Secret::USERNAME, Secret::PASSWORD )
 		p "Sending 'on'"
 		@im.deliver( 'twitter@twitter.com', 'on' )
-		#p "Sending 'help'"
-		#@im.deliver( 'twitter@twitter.com', 'help' )
-		p "Sending 'track'"
-		@im.deliver( 'twitter@twitter.com', 'track' )
+		#p "Sending 'track'"
+		#@im.deliver( 'twitter@twitter.com', 'track' )
 	end
 	
 	def run
 		while true
-			begin
+			#begin
 				receive
-				sleep 10
-			rescue
-				p "Exception raised!"
-			end
+				sleep 1
+			#rescue
+			#	p "Exception raised!"
+			#end
 		end
 	end
 	
 	#def readupdates
-	#	File.open( 'tweets/tweets.js', 'r' ) do |f|
+	#	File.open( @JSON, 'r' ) do |f|
 	#		data = f.read
 	#		oldUpdates = JSON.parse( data )
 	#		oldUpdates.each { |update|
@@ -53,24 +52,27 @@ class Updater
 	#end
 	
 	def writeupdates
-		File.open( 'tweets/tweets.js', 'w' ) do |f|
-			f.puts @updatelist.to_json
+		p 'Writing updates'
+		# Add newlines to JSON output to make it more Subversion-friendly
+		json = @updatelist.to_json.sub( /^\[/, "[\n" ).sub( /\]\s*$/, "\n]\n" ).gsub( /"\},\{"/, "\"},\n{\"" )
+		File.open( @JSON, 'w' ) do |f|
+			f.puts json
 		end
 		#p 'Checking in updates'
-		#`svn ci -m "Twitter update" tweets/tweets.js`
+		#`svn ci -m "Twitter update" #{@JSON}`
 		#p 'Done checking in'
 	end
 	
 	#readupdates
 	
 	def onemsg( msg )
-		p msg.body
+		#p msg.body
 		return if msg.type != :chat or msg.from != 'twitter@twitter.com' or @updates[msg.body]
 		body = msg.body
-		match = /^\s*\((.*)\):(.*)$/.match(body)
+		match = /^(.*):(.*)$/.match(body)
 		return if not match
-		username = match[1]
-		message = match[2]
+		username = match[1].strip
+		message = match[2].strip
 		if Banned.banned(body)
 			p "Blocked: #{message}"
 			return
@@ -92,7 +94,8 @@ class Updater
 		@updates[msg.body] = update
 		@updatelist.push( update )
 		@updatelist.delete_at(0) if @updatelist.length > @MAX_UPDATES
-		if Time.now - @lastwrite > 300 and @updatelist.length >= 20
+		#if Time.now - @lastwrite > 300 and @updatelist.length >= 20
+		if @updatelist.length >= 5
 			@lastwrite = Time.now
 			writeupdates
 		end
@@ -100,7 +103,8 @@ class Updater
 	
 	def getuser( username, author )
 		if not @users[username]
-			http = Net::HTTP.new( 'api.twittervision.com' )
+			#p "Getting twittervision user #{username}"
+			http = Net::HTTP.new( 'twittervision.com' )
 			headers, body = http.get( "/user/current_status/#{username}.xml" )
 			if headers.code == '200'
 				tv = Hpricot::XML(body)
@@ -127,7 +131,7 @@ class Updater
 	def receive
 		p "Start receive"
 		@im.received_messages do |msg|
-			p "Got msg: #{msg}"
+			#p "Got msg: #{msg}"
 			onemsg msg
 		end
 	end
