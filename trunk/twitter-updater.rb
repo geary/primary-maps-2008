d018f328b4b16d7f825739366811ecb3e066e5f1
mg@mg.to <mg@mg.to@d6bc2760-3145-0410-a777-cddf8f24c489>
2008-07-15 01:45:27 +0000

    Make tweets-latest.js public on AS3

--- trunk/twitter-updater.rb
+++ trunk/twitter-updater.rb
@@ -106,7 +106,7 @@ class Updater
 			:access_key_id     => Secret::S3_KEY,
 			:secret_access_key => Secret::S3_SECRET
 		)
-		AWS::S3::S3Object.store( "twitter/#{@JSON}", open(@JSON), 'elections' )
+		AWS::S3::S3Object.store( "twitter/#{@JSON}", open(@JSON), 'elections', :access => :public_read )
 		print "Done uploading\n"
 	end
 	
