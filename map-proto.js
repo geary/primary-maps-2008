a391e0b1a375c802d7393fb83b8a880ba40138e1
election.maps.2008 <election.maps.2008@d6bc2760-3145-0410-a777-cddf8f24c489>
2008-08-15 19:13:50 +0000

    Remove "current primary states" option

--- map-proto.js
+++ map-proto.js
@@ -1832,9 +1832,9 @@ opt.codeUrl = opt.codeUrl || 'http://primary-maps-2008.googlecode.com/svn/trunk/
 opt.frameUrl = opt.frameUrl || opt.codeUrl;
 opt.dataUrl = opt.dataUrl || 'http://primary-maps-2008-data.googlecode.com/svn/trunk/';
 opt.state = opt.state || 'us';
-if( opt.state == 'zz' )
-	//opt.state = hotStates.random().replace( '!', '' ).toLowerCase();
-	opt.state = 'us';
+//if( opt.state == 'zz' )
+//	//opt.state = hotStates.random().replace( '!', '' ).toLowerCase();
+//	opt.state = 'us';
 
 var state = states[opt.state];
 
